3df22181b4f180f5642f9fe8aa544e3f0727b229
OGHyper <firefoxphil@hotmail.com>
2023-11-23 17:04:38 -0500

    Added early implementation of the CourseReview class

--- src/main/java/edu/virginia/sde/reviews/CourseReview.java
+++ src/main/java/edu/virginia/sde/reviews/CourseReview.java
@@ -1,4 +1,53 @@
 package edu.virginia.sde.reviews;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class CourseReview {
+    private Course course;
+    private Student postingStudent;
+    private double rating;
+    private String comment;
+    private Timestamp timestamp;
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public Student getPostingStudent() {
+        return postingStudent;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public void setPostingStudent(Student postingStudent) {
+        this.postingStudent = postingStudent;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setTimestamp(Date date){
+        this.timestamp = new Timestamp(date.getTime());
+    }
 }
