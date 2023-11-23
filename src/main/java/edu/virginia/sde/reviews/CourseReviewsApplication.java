e9cf6769da7fdf0b53d3636e9504dbb4cb0e634e
OGHyper <firefoxphil@hotmail.com>
2023-11-23 16:44:16 -0500

    Created a bunch of files and early implementation of the Student class

---
+++ src/main/java/edu/virginia/sde/reviews/CourseReviewsApplication.java
@@ -0,0 +1,12 @@
+package edu.virginia.sde.reviews;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CourseReviewsApplication extends Application {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/edu/virginia/sde/reviews/Student.java
@@ -0,0 +1,37 @@
+package edu.virginia.sde.reviews;
+
+import java.util.*;
+
+public class Student {  // Basically the user
+    // What we need:
+    // Username
+    private String username;
+    // Password
+    private String password;
+    // List of reviews
+    List<CourseReview> reviews; // This might need more to instantiate
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<CourseReview> getReviews() {
+        return reviews;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setReviews(List<CourseReview> reviews) {
+        this.reviews = reviews;
+    }
+}
